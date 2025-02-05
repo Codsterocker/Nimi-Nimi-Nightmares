5439b04a1f11f5987a5ec5c724e440344927f12c
Codsterocker <codyperdue88@hotmail.com>
2025-02-04 20:28:47 -0500

    Main Menu Adjustments

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject PressToPlayPanel;
-    public GameObject MainMenuPanel;
+    public GameObject pressToPlayPanel;
+    public GameObject mainMenuPanel;
+
+    public GameObject pressToPlayFirstSelected;
+    public GameObject mainMenuFirstSelected;
+
+    private GameObject firstSelectedGameObject;
 
     private void Start()
     {
         ShowPressToPlayPanel();
+    }
+
+    void Update()
+    {
+        // Ensure that the controller is being used
+        if (Input.GetJoystickNames().Length > 0 || Input.GetKey(KeyCode.JoystickButton0))
+        {
+            // Force EventSystem to focus on the controller-selected object
+            if (EventSystem.current.currentSelectedGameObject == null)
+            {
+                EventSystem.current.SetSelectedGameObject(firstSelectedGameObject);
+            }
+        }
     }
 
     public void PlayGame()
@@ -25,15 +44,30 @@ public class MainMenu : MonoBehaviour
         #endif
     }
 
+    public void ShowSettingsPanel()
+    {
+        Debug.Log("Show Settings Hit");
+    }
+
     public void ShowPressToPlayPanel()
     {
-        PressToPlayPanel.SetActive(true);
-        MainMenuPanel.SetActive(false);
+        // Set the active panel
+        pressToPlayPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+
+        // Set the first menu option to be selected
+        firstSelectedGameObject = pressToPlayFirstSelected;
+        EventSystem.current.SetSelectedGameObject(firstSelectedGameObject);
     }
 
     public void ShowMainMenu()
     {
-        PressToPlayPanel.SetActive(false);
-        MainMenuPanel.SetActive(true);
+        // Set the active panel
+        pressToPlayPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+
+        // Set the first menu option to be selected
+        firstSelectedGameObject = mainMenuFirstSelected;
+        EventSystem.current.SetSelectedGameObject(firstSelectedGameObject);
     }
 }
