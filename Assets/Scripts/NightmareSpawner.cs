14c974d9376e0bcb340e9a375dc4e4746bfbe5a4
Codsterocker <codyperdue88@hotmail.com>
2025-02-06 00:50:46 -0500

    Player + Spawner Changes
    - Player now uses Input Actions
    - Set up Input Actions with Blow / Suck
    - Blow / Suck functionality added
    - Added score tracker to Player
    - Nightmare spawner now has nightmares move into arena

--- Assets/Scripts/NightmareSpawner.cs
+++ Assets/Scripts/NightmareSpawner.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class RandomEdgeSpawner : MonoBehaviour
+public class NightmareSpawner : MonoBehaviour
 {
     public GameObject objectToSpawn;    // The GameObject to spawn
     public GameObject objectParent;     // The parent the spawn objects will be created under
@@ -9,10 +9,12 @@ public class RandomEdgeSpawner : MonoBehaviour
 
     public float waitTimeLow = 1f;      // The minimum wait time (seconds)
     public float waitTimeHigh = 5f;     // The maximum wait time (seconds)
+    public float spawnForceLow = 1f;    // The minimum force of the nightmare spawning
+    public float spawnForceHigh = 5f;   // The maximum force of the nightmare spawning
 
     private float radius;               // The radius of the circle
     private float elapsedTime = 0f;     // Time elapsed since the script started
-    private float timeUntilStop;         // Seconds until timer ends
+    private float timeUntilStop;        // Seconds until timer ends
 
     private void Start()
     {
@@ -41,6 +43,17 @@ public class RandomEdgeSpawner : MonoBehaviour
             // Instantiate the object and set its parent under objectParent
             GameObject spawnedObject = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
             spawnedObject.transform.SetParent(objectParent.transform);
+
+            // Add a random force to the spawned object back into the arena
+            Rigidbody rb = spawnedObject.GetComponent<Rigidbody>();
+            float returnAngle = Random.Range(-70, 70);
+            float spawnStrength = Random.Range(spawnForceLow, spawnForceHigh);
+
+            Vector3 spawnDirection = (transform.position - spawnedObject.transform.position);
+            Quaternion spawnRotation = Quaternion.AngleAxis(returnAngle, Vector3.up);
+            Vector3 spawnForce = (spawnRotation * spawnDirection) * spawnStrength;
+
+            spawnedObject.GetComponent<Rigidbody>().AddForce(spawnForce);
 
             // Wait for a random time between waitTimeLow and waitTimeHigh
             float waitTime = Random.Range(waitTimeLow, waitTimeHigh);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,72 +4,134 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 50f;   // How fast the player moves
-    public Camera arenaCamera;
+    public float moveSpeed = 50f;       // How fast the player is able to move
+    public float blowStrength = 10f;    // How powerful the nightmares will be blown back
+    public float actionTimer = 1f;      // How often the player can suck/blow
+    public Camera arenaCamera;          // The main camera, so movement will be based on its position if it moves
+    public int playerScore = 0;         // The players total amount of captured nightmares
 
+    private Rigidbody rigidBody;                        // Reference to the player's Rigidbody 
+    private Vector3 forceDirection = Vector3.zero;      // The force that will be applied to the rb
+    private float actionTimeSinceActivated = 0;         // Time since Blow/Suck was last activated
+
+    // Input Action variables
     private InputActionAsset inputActionAsset;
     private InputActionMap player;
     private InputAction move;
-
-    private Rigidbody rigidBody;                        // Reference to the player's Rigidbody 
-    private Vector3 forceDirection = Vector3.zero;      // the force that will be applied to the rb
 
     private void Awake()
     {
         // Get the Rigidbody component on the player
         rigidBody = GetComponent<Rigidbody>();
 
+        // Set up the Input Actions for this player
         inputActionAsset = this.GetComponent<PlayerInput>().actions;
         player = inputActionAsset.FindActionMap("Player");
     }
 
+    // Enable this player's controls
     private void OnEnable()
     {
         move = player.FindAction("Move");
+        player.FindAction("Suck").started += DoSuck;
+        player.FindAction("Blow").started += DoBlow;
         player.Enable();
     }
 
+    // Disable this player's controls
     private void OnDisable()
     {
+        player.FindAction("Suck").started -= DoSuck;
+        player.FindAction("Blow").started -= DoBlow;
         player.Disable();
-    }
-
-    void Start()
-    {
-        
     }
 
     private void Update()
     {
-        
+        actionTimeSinceActivated += Time.deltaTime;
     }
 
+    // Updates every fixed framerate frame
     void FixedUpdate()
     {
         MovePlayer();
     }
 
+    // Calculate the movement for the player
     void MovePlayer()
     {
+        // Read the player's inputs
         Vector2 playerMovement = move.ReadValue<Vector2>();
 
+        // Calculate the movement based on the camera position
         forceDirection += move.ReadValue<Vector2>().x * GetCameraRight(arenaCamera) * moveSpeed;
         forceDirection += move.ReadValue<Vector2>().y * GetCameraForward(arenaCamera) * moveSpeed;
 
+        // Add the force to the object
         rigidBody.AddForce(forceDirection, ForceMode.Impulse);
         forceDirection = Vector3.zero;
     }
 
-    private Vector3 GetCameraRight(Camera arenaCamera)
+    // Suck up the nightmares within the zone range
+    private void DoSuck(InputAction.CallbackContext context)
     {
-        Vector3 right = arenaCamera.transform.right;
+        if (actionTimeSinceActivated > actionTimer)
+        {
+            Collider[] collectibles = Physics.OverlapSphere(transform.position, transform.localScale.x / 2);
+
+            foreach (Collider collectible in collectibles)
+            {
+                if (collectible.CompareTag("Collectible"))
+                {
+                    playerScore++;
+                    Destroy(collectible.gameObject);
+                }
+            }
+
+            actionTimeSinceActivated = 0f;
+        }
+
+        Debug.Log("Sucked");
+    }
+
+    // Push back the nightmares within the zone range
+    private void DoBlow(InputAction.CallbackContext context)
+    {
+        if (actionTimeSinceActivated > actionTimer)
+        {
+            Collider[] collectibles = Physics.OverlapSphere(transform.position, transform.localScale.x / 2);
+
+            foreach (Collider collectible in collectibles)
+            {
+                if (collectible.CompareTag("Collectible"))
+                {
+                    Rigidbody rb = collectible.GetComponent<Rigidbody>();
+                    Vector3 blowDirection = (collectible.transform.position - transform.position).normalized;
+
+                    Vector3 blowForce = blowDirection * blowStrength;
+
+                    rb.AddForce(blowForce);
+                }
+            }
+
+            actionTimeSinceActivated = 0f;
+        }
+
+        Debug.Log("Blown");
+    }
+
+    // Grab the camera's right parameters
+    private Vector3 GetCameraRight(Camera camera)
+    {
+        Vector3 right = camera.transform.right;
         right.y = 0f;
         return right.normalized;
     }
 
-    private Vector3 GetCameraForward(Camera arenaCamera)
+    // Grab the camera's forward parameters
+    private Vector3 GetCameraForward(Camera camera)
     {
-        Vector3 forward = arenaCamera.transform.forward;
+        Vector3 forward = camera.transform.forward;
         forward.y = 0f;
         return forward.normalized;
     }
